95db253d7cbff568a71f9f2fb324da37757f7229
Michal `acid` Bagrowski <michal@bagrowski.com>
2018-08-09 12:57:55 +0200

    wip

--- terraform_wip/openstack.tf
+++ terraform_wip/openstack.tf
@@ -1,63 +1,95 @@
 provider "openstack" {
 }
 
+variable "env_name" {
+  default = "test"
+}
 
+variable "ext-net" {
+  default = "a07d7f52-0eed-4242-91fd-368dea025cc7"
+}
 
-resource "openstack_networking_network_v2" "test" {
-  name = "acid-test"
-  external = true
+variable "image_id" {
+  default = "d324da4b-b827-44fb-8331-bfad41a28aca"
+}
+
+variable "flavor_name" {
+  default = "c1.medium"
+}
+
+variable "key_name" {
+  default = "CircleCI Master"
+}
+
+resource "openstack_networking_network_v2" "network" {
+  name           = "ae-${var.env_name}-net"
+  admin_state_up = "true"
+}
+
+resource "openstack_networking_subnet_v2" "subnet" {
+  name       = "ae-${var.env_name}-subnet"
+  network_id = "${openstack_networking_network_v2.network.id}"
+  cidr       = "192.168.60.0/24"
+  ip_version = 4
+  enable_dhcp = true
+  dns_nameservers = ["1.1.1.1","1.0.0.1"]
+}
+
+resource "openstack_networking_router_v2" "router" {
+  name       = "ae-${var.env_name}-router"
+  admin_state_up      = true
+  external_network_id = "${var.ext-net}"
+}
+
+resource "openstack_networking_router_interface_v2" "router_interface" {
+  router_id = "${openstack_networking_router_v2.router.id}"
+  subnet_id = "${openstack_networking_subnet_v2.subnet.id}"
+}
+
+resource "openstack_networking_port_v2" "port" {
+  name       = "ae-${var.env_name}-port"
+  network_id     = "${openstack_networking_network_v2.network.id}"
+  admin_state_up = true
+  security_group_ids = [
+    "${openstack_compute_secgroup_v2.management.id}",
+    "${openstack_compute_secgroup_v2.epoch.id}",
+  ]
 }
 
 
+resource "openstack_compute_secgroup_v2" "management" {
+  name = "ae-${var.env_name}-management"
+  description = "Allow node management from internet (ICMP, SSH, API)"
+}
 
-resource "openstack_compute_instance_v2" "test" {
+resource "openstack_compute_secgroup_v2" "epoch" {
+  name = "ae-${var.env_name}-epoch"
+  description = "Full access between epoch nodes in their environment"
+}
+
+resource "openstack_compute_secgroup_v2" "internal" {
+  name = "ae-${var.env_name}-internal"
+  description = "Allow access to internal ports"
+}
+
+
+resource "openstack_compute_instance_v2" "basic" {
   count = 2
-  name = "acid-test-${count.index}"
-  image_id = "5566886d-0d9d-4d86-94ff-55597169e15a"
-  flavor_name = "c1.tiny"
+  name            = "ae-${var.env_name}-epoch-n${count.index}"
+  image_id        = "${var.image_id}"
+  flavor_name       = "${var.flavor_name}"
+  key_pair        = "${var.key_name}"
+  security_groups = [
+    "${openstack_compute_secgroup_v2.management.id}",
+    "${openstack_compute_secgroup_v2.epoch.id}",
+    "${openstack_compute_secgroup_v2.internal.id}",
+  ]
+
+  metadata {
+    this = "that"
+  }
 
   network {
-    name = "${openstack_networking_network_v2.test.name}"
-  }
-
-  security_groups = ["${openstack_compute_secgroup_v2.test.name}"]
-}
-
-
-resource "openstack_networking_floatingip_v2" "ip" {
-  count = 2
-  pool = "acid-test"
-}
-
-resource "openstack_compute_floatingip_associate_v2" "ips" {
-  count = 2
-  floating_ip = "${element(openstack_networking_floatingip_v2.ip.*.address, count.index)}"
-  instance_id = "${element(openstack_compute_instance_v2.test.*.id, count.index)}"
-}
-
-resource "openstack_networking_subnet_v2" "test" {
-  name       = "acid-test_1"
-  network_id = "${openstack_networking_network_v2.test.id}"
-  cidr       = "10.10.0.0/24"
-  ip_version = 4
-}
-
-
-resource "openstack_compute_secgroup_v2" "test" {
-  name        = "acid-test"
-  description = "acid-test security group"
-
-  rule {
-    from_port   = 22
-    to_port     = 22
-    ip_protocol = "tcp"
-    cidr        = "0.0.0.0/0"
-  }
-
-  rule {
-    from_port   = 80
-    to_port     = 80
-    ip_protocol = "tcp"
-    cidr        = "0.0.0.0/0"
+    name = "ext-net"
   }
 }
